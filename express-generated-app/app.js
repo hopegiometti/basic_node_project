ffe93eee1c341d2d87272adf711d6ff2771c4dbb
Hope <hopegiometti@gmail.com>
2020-06-24 15:03:02 -0400

    changed port

--- express-generated-app/app.js
+++ express-generated-app/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const port = process.env.PORT || "8000";
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+app.get("/", (req, res)=> {
+  res.status(200).send("WHATABYTE: Food For Devs")
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,6 +42,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
 });
 
 module.exports = app;
