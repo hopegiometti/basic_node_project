24e1e210feed46a1e2f81ad10e4b6ab1c5f36eb5
Hope <hopegiometti@gmail.com>
2020-06-24 15:09:56 -0400

    trying Pug

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
 // let log = require('./logger')
 
@@ -11,6 +11,47 @@ const app = express()
 
 // log('hello world')
 
-app.get('/', function(req, res) {
-    res.send('hello world')
-}).listen(3000)
+// app.get('/', function(req, res) {
+//     res.send('hello world')
+// }).listen(3000)
+
+/**
+ * Required External Modules
+ */
+
+const express = require("express");
+const path = require("path");
+
+/**
+ * App Variables
+ */
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+/**
+ *  App Configuration
+ */
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+/**
+ * Routes Definitions
+ */
+
+// app.get("/", (req, res) => {
+//     res.status(200).send("WHATABYTE: Food For Devs");
+// });
+
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+});
+
+/**
+ * Server Activation
+ */
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
